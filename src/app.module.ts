8dae13c0d77604a50ecaf999d70a7faf6972f54d
Armando <armandoalvovi7777@gmail.com>
2025-01-16 01:32:32 -0600

    feat: initial setup and database connection

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+}
